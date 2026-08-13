107d3c7433660dbda5e2c8bb6ae5096a20762803
Adam Lekh <en0tadam@gmail.com>
2026-08-13 16:49:50 +0300

    init commit

---
+++ src/server.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import 'dotenv/config';
+import PinoHttp from 'pino-http';
+import cors from 'cors';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(express.json());
+
+const logger = PinoHttp({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss',
+      ignore: 'pid,hostname',
+      messageFormat:
+        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+      hideObject: true,
+    },
+  },
+});
+app.use(logger);
+
+app.use(cors());
+
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((error, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const message = isProd ? 'Some error' : error.message;
+  res.status(500).json({
+    message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
