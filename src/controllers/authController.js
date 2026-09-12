bba90ccda7b7bcd6934588e8fbe41f8777803073
Adam Lekh <en0tadam@gmail.com>
2026-09-12 22:14:56 +0300

    fix: rename sendEmail.js to sendMail.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import fs from 'node:fs/promises';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
